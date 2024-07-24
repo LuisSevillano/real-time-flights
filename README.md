23e26d2440b32752de781cc33f760be6541711bc
LuisSevillano <luissevillanopires@gmail.com>
2024-07-24 11:06:17 +0200

    Automate video generation

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # Real time world flights situation
 
 Getting data from [flight/](https://www.flightradar24.com/40.41,-4.12/7) to check if a zone changes over time.
+
+![flights gif](./video/flights.gif)
---
+++ generate_video.sh
@@ -0,0 +1,11 @@
+mkdir -p video
+
+ffmpeg -y \
+    -pattern_type glob -i 'images/*.jpg' \
+    -vf "fps=15,format=yuv420p" \
+    video/flights.mp4
+
+ffmpeg -y \
+    -i flights.mp4 \
+    -vf "fps=30,scale=1200:-1:flags=lanczos,split[s0][s1];[s0]palettegen[p];[s1][p]paletteuse" \
+    -loop 0 video/flights.gif
