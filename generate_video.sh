c526f821fcf96322a973d4057117681ad8d1cd08
LuisSevillano <luissevillanopires@gmail.com>
2024-07-25 14:05:37 +0200

    Fix issue with timestamp

--- generate_video.sh
+++ generate_video.sh
@@ -4,8 +4,7 @@
 mkdir -p video
 
 # Get current date and time in milliseconds
-current_time="$(date +%s%3)00"
-current_time=$(($current_time))
+current_time=$(($(date +%s) * 1000))
 echo $current_time
 
 # Filter images from the last 24 hours (86400000 milliseconds)
